3a7e041944cd0b8679dfb0977441922701fdcc82
Raul Ismagilov <smaggy@mail.ru>
2024-08-03 21:19:13 +0300

    Счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Leibl: UILabel!
+    @IBOutlet weak var Knopka: UIButton!
+    var number = 0
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Leibl.text = "Значение: \(number)"
         // Do any additional setup after loading the view.
     }
+    
 
-
-}
+    @IBAction func Perekl(_ sender: UIButton) {
+        number += 1
+        Leibl.text = "Значение: \(number)"
+    }
+    
+    }
+    
+    
 
