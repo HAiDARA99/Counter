d3b9edfc8b89a4e305c97231939408233d153159
Raul Ismagilov <smaggy@mail.ru>
2024-08-10 22:00:28 +0300

    Полностью дописан счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var Leibl: UILabel!
     @IBOutlet weak var Knopka: UIButton!
+    @IBOutlet weak var Minus: UIButton!
+    @IBOutlet var Sbros: UIView!
+    @IBOutlet weak var TextPole: UITextView!
     var number = 0
+    
+    
+ 
+    
 
     
     override func viewDidLoad() {
@@ -20,11 +27,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
 
     @IBAction func Perekl(_ sender: UIButton) {
         number += 1
         Leibl.text = "Значение: \(number)"
+        TextPole.text = "\(Date()): значение изменено на +1"
     }
+    
+    @IBAction func Minusit(_ sender: UIButton) {
+        if number <= 0 {
+            number = 0
+        } else {
+            number -= 1
+        }
+        Leibl.text = "Значение: \(number)"
+        TextPole.text = "\(Date()): значение изменено на -1"
+    }
+    
+    
+    @IBAction func Sbrosit(_ sender: Any) {
+        number = 0
+        Leibl.text = "Значение: \(number)"
+        TextPole.text = "\(Date()): значение сброшено"
+    }
+    
+    
+    
+    
     
     }
     
